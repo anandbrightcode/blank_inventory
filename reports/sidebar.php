59d324d1618ac75ad706b07fe2abe5e1234783d2
anandbrightcode <72801448+anandbrightcode@users.noreply.github.com>
2023-06-15 17:33:19 +0530

    Update sidebar.php

--- reports/sidebar.php
+++ reports/sidebar.php
@@ -1,9 +1,9 @@
 <a href="../reports/?pagename=report" class="btn btn-success" style="width:100%">Invoice</a><br /><br />
-<a href="invoiceservice.php?pagename=report" class="btn btn-warning" style="width:100%"> Service Invoice</a><br /><br />
+<!-- <a href="invoiceservice.php?pagename=report" class="btn btn-warning" style="width:100%"> Service Invoice</a><br /><br /> -->
 <a href="dues.php?pagename=report" class="btn btn-danger" style="width:100%">Dues</a><br /><br />
-<a href="performa.php?pagename=report" class="btn btn-info" style="width:100%">Perform Invoice</a><br /><br />
-<a href="quotation.php?pagename=report" class="btn btn-primary" style="width:100%">Quotation</a><br /><br />
-<a href="qutoservice.php?pagename=report" class="btn btn-warning" style="width:100%">Quotation Invoice</a><br /><br />
+<!-- <a href="performa.php?pagename=report" class="btn btn-info" style="width:100%">Perform Invoice</a><br /><br /> -->
+<!-- <a href="quotation.php?pagename=report" class="btn btn-primary" style="width:100%">Quotation</a><br /><br /> -->
+<!-- <a href="qutoservice.php?pagename=report" class="btn btn-warning" style="width:100%">Quotation Invoice</a><br /><br /> -->
 <a href="purchase_order.php?pagename=report" class="btn btn-info" style="width:100%">Purchase</a><br /><br />
 <a href="customer.php?pagename=report" class="btn btn-primary" style="width:100%">Customer</a><br /><br />
 <a href="supplier.php?pagename=report" class="btn btn-info" style="width:100%">Supplier</a><br /><br />
@@ -14,7 +14,7 @@
 <a href="profitloss.php?pagename=report" class="btn btn-primary" style="width:100%">Profit / Loss</a><br /><br />
 <a href="taxes.php?pagename=report" class="btn btn-info" style="width:100%">Duties &amp; Taxes</a><br /><br />
 <a href="balancesheet.php?pagename=report" class="btn btn-primary" style="width:100%">Balance Sheet</a><br /><br />
-<a href="../backup/" class="btn btn-info" style="width:100%" onClick="createBackup();">Create Backup <i class="fa fa-plus"></i></a><br /><br />
+<!-- <a href="../backup/" class="btn btn-info" style="width:100%" onClick="createBackup();">Create Backup <i class="fa fa-plus"></i></a><br /><br /> -->
 <script>
 	function createBackup(){
 		alert("Copy and Keep the Downloaded Backup File in a Different Drive!");	
